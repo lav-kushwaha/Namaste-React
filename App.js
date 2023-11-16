e97d37b6d0ebcec65a456c954d360659b6a48ed5
Lav Kushwaha <lavkumar1224@gmail.com>
2023-11-16 20:17:16 +0530

    Creating React Root

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-const heading = React.createElement('h1',{},'Hello World From React');
+const heading = React.createElement(
+'h1',
+{id:"heading", className:"attribute"},
+'Hello World From React!'
+);
+
+console.log(heading); //object
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
+
+
+
+// This is attribute.
+// {id:"heading", className:"attribute"}
