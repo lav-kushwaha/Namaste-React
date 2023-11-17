658114948cbcfdbeed0c68fd2039a998a27f4476
Lav Kushwaha <lavkumar1224@gmail.com>
2023-11-17 20:27:54 +0530

    commit

--- Chapter 01 - Inception/App.js
+++ Chapter 01 - Inception/App.js
@@ -10,7 +10,7 @@
 
 // This is attribute.
 // {id:"heading", className:"attribute"}
-// =======================================++++++++++++==================================================
+// ===================================++++++++++++==================================================
 /*  
 <div id="parent">
     <div id="child">
@@ -27,11 +27,10 @@
 ReactElement(object) => HTM(Browser Understands)
 */
 
+// ==================================================++++++++++++++++++++++++======================================
 
-// ==========================================================++++++++++++++++++++++++======================================
-
-//Nested Element.
-// ==> createElement takes 3 argument name of the tag like h1,p,div,span etc, and 2nd agrument is attribute it takes id class etc, 3rd argument takes children(content)
+//Creating Nested Element.
+// ==> React.createElement takes 3 argument 1st name of the tag like h1,p,div,span etc, and 2nd argument is attribute it takes id class etc, 3rd argument takes children(content)
 const parent = React.createElement('div',{id:"parent"},
 React.createElement('div',{id:"child"},[React.createElement('h1',{},"Hello I am an H1 tag"),React.createElement('h2',{},"Hello I am an H2 tag")]),
 React.createElement('div',{id:"child2"},[React.createElement('h1',{},'Hello I am child2 h1'),React.createElement('h2',{},"Hello I am Child2 h2")])
@@ -40,7 +39,7 @@ React.createElement('div',{id:"child2"},[React.createElement('h1',{},'Hello I am
 //object
 console.log(parent);
 
-// Accessing DOM div id root by using ReactDOM.createRoot.
+// Accessing DOM div id="root" by using ReactDOM.createRoot.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //when we put parent-object into the root.render the render method. it is used to convert parent-object into HTML inside the dom.
